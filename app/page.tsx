81b888e270a1a1764c888eb52c0b269d1450bc8d
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-07-12 15:40:47 +0530

    form input n store modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store.modal";
 
 const Home = () => {
-  return (
-    <div className='p-4'>
-      <Modal title='Test' description='Test desc' isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal(state => state.onOpen);
+  const isOpen = useStoreModal(state => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
+  return <div className='p-4'>Root page</div>;
 };
 
 export default Home;
